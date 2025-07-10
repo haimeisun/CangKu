16627c59dc0cb98f347603d01dab087b6ef60407
haimeisun <1802307551@qq.com>
2025-07-10 18:50:11 +0800

    仓库管理项目

---
+++ cangku_project/src/main/java/com/wms/controller/MenuController.java
@@ -0,0 +1,36 @@
+package com.wms.controller;
+
+
+import com.wms.common.Result;
+import com.wms.entity.Menu;
+import com.wms.entity.User;
+import com.wms.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 菜单管理的前端控制器，处理与菜单相关的HTTP请求
+ * 根据角色ID获取菜单列表
+ * </p>
+ *
+ * @author wms
+ */
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+    @Autowired
+    private MenuService menuService;
+    @GetMapping("/list")
+    public Result list(@RequestParam String roleId){
+        List list = menuService.lambdaQuery().like(Menu::getMenuright,roleId).list();
+        return Result.suc(list);
+    }
+}
---
+++ cangku_project/src/test/java/com/cangku/cangku_project/CangkuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cangku.cangku_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CangkuProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
